e21c53cf0307b457de491872b9c7726025845aae
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-11-29 20:12:03 +0200

    Update finaldefectapp_py.py

--- finaldefectapp_py.py
+++ finaldefectapp_py.py
@@ -12,7 +12,6 @@ def load_mobilenet_model():
     model = load_model(model_path)
     return model
 
-
 # Function to make predictions
 def predict_defect(image_path, model):
     img = image.load_img(image_path, target_size=(150, 150))
@@ -32,12 +31,13 @@ def main():
     if uploaded_file is not None:
         st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
 
-    # Create a temporary directory if it doesn't exist
-    temp_dir = 'temp'
-    os.makedirs(temp_dir, exist_ok=True)
+        # Create a temporary directory if it doesn't exist
+        temp_dir = 'temp'
+        os.makedirs(temp_dir, exist_ok=True)
 
-    # Create a path for the temporary image
-    temp_path = os.path.join(temp_dir, 'temp_image.jpg')
+        # Create a path for the temporary image
+        temp_path = os.path.join(temp_dir, 'temp_image.jpg')
+
         uploaded_file.seek(0)
         with open(temp_path, 'wb') as f:
             f.write(uploaded_file.read())
@@ -50,6 +50,7 @@ def main():
 
         # Display the results
         st.subheader("Prediction Results:")
+        classes = ["Crazing", "Inclusion", "Patches", "Pitted", "Rolled", "Scratches"]
         for i, class_name in enumerate(classes):
             st.write(f"{class_name}: {prediction[0][i]}")
 
