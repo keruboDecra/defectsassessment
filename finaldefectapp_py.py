f51f3caa811cb52b30d66a111dc3461465cba7ac
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-11-29 19:41:24 +0200

    Update finaldefectapp_py.py

--- finaldefectapp_py.py
+++ finaldefectapp_py.py
@@ -2,46 +2,50 @@
 from keras.models import load_model
 from keras.preprocessing import image
 
-# Load the trained MobileNet model
-model = 'mobilenet_model(1).h5'
+# Load the trained model
+model = 'mobilenet_model(1).h5'  # Update with the actual path
 
-# Define the input size for the model
-img_width, img_height = 150, 150
+# Define the defect classes
+classes = ['Crazing', 'Inclusion', 'Patches', 'Pitted', 'Rolled', 'Scratches']
 
-# Function to preprocess the image for prediction
-def preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(img_width, img_height))
+# Function to preprocess the input image
+def preprocess_image(img_path, target_size=(150, 150)):
+    img = image.load_img(img_path, target_size=target_size)
     img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0  # Normalize pixel values
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0  # Normalize pixel values
     return img_array
 
-# Streamlit app
-st.title("Defects Assessment App")
-
-# Upload image through Streamlit
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-
-# Make a prediction when an image is uploaded
-if uploaded_file is not None:
-    # Preprocess the image
-    img = preprocess_image(uploaded_file)
+# Function to make predictions
+def predict_defect(image_path):
+    # Preprocess the input image
+    processed_img = preprocess_image(image_path)
 
     # Make prediction
-    prediction = model.predict(img)
+    prediction = model.predict(processed_img)
 
     # Get the predicted class
-    predicted_class = np.argmax(prediction)
-    classes = ['Crazing', 'Inclusion', 'Patches', 'Pitted', 'Rolled', 'Scratches']
-    predicted_class_name = classes[predicted_class]
+    predicted_class = classes[np.argmax(prediction)]
 
-    # Display the image and prediction
-    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
-    st.write("Class Prediction: ", predicted_class_name)
-    st.write("Confidence: {:.2f}%".format(prediction[0][predicted_class] * 100))
+    return predicted_class
 
-    # Bar chart for class probabilities
-    fig, ax = plt.subplots()
-    ax.bar(classes, prediction[0] * 100)
-    ax.set_ylabel('Probability (%)')
-    ax.set_title('Class Probabilities')
-    st.pyplot(fig)
+# Streamlit app
+def main():
+    st.title("Defects Assessment App")
+    st.write("Upload an image to assess defects.")
+
+    # Upload image through Streamlit
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+    if uploaded_file is not None:
+        # Display the uploaded image
+        st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+
+        # Make prediction
+        predicted_class = predict_defect(uploaded_file)
+
+        st.write(f"Prediction: {predicted_class}")
+
+# Run the app
+if __name__ == '__main__':
+    main()
