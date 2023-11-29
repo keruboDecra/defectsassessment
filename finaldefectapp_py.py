6ec4eca62e25807974cb5b832e5d9caaad94e92b
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-11-29 19:37:52 +0200

    Update finaldefectapp_py.py

--- finaldefectapp_py.py
+++ finaldefectapp_py.py
@@ -1,48 +1,47 @@
 # Import necessary libraries
+from keras.models import load_model
 from keras.preprocessing import image
-from keras.applications.mobilenet import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from PIL import Image
 
 # Load the trained MobileNet model
 model = 'mobilenet_model(1).h5'
 
-# Image preprocessing function
-def preprocess_image(img):
-    img = img.resize((150, 150))  # Resize image to match the input size of the model
+# Define the input size for the model
+img_width, img_height = 150, 150
+
+# Function to preprocess the image for prediction
+def preprocess_image(img_path):
+    img = image.load_img(img_path, target_size=(img_width, img_height))
     img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
+    img_array = np.expand_dims(img_array, axis=0) / 255.0  # Normalize pixel values
     return img_array
 
-# Prediction function
-def predict_defect(img_array):
-    prediction = model.predict(img_array)
-    return prediction
-
-# Streamlit UI
-st.title('Defects Assessment')
+# Streamlit app
+st.title("Defects Assessment App")
 
 # Upload image through Streamlit
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
+# Make a prediction when an image is uploaded
 if uploaded_file is not None:
-    # Display the uploaded image
-    img = Image.open(uploaded_file)
-    st.image(img, caption='Uploaded Image.', use_column_width=True)
-
     # Preprocess the image
-    img_array = preprocess_image(img)
+    img = preprocess_image(uploaded_file)
 
-    # Make a prediction
-    prediction = predict_defect(img_array)
+    # Make prediction
+    prediction = model.predict(img)
 
-    # Display the prediction result
-    st.write("Prediction:")
-    st.write(prediction)
-
-    # You can further process the prediction result to display a meaningful output based on your classes.
-    # For example, if you have classes like ['Crazing', 'Inclusion', 'Patches', 'Pitted', 'Rolled', 'Scratches'],
-    # you can display the predicted class with the highest probability.
+    # Get the predicted class
     predicted_class = np.argmax(prediction)
-    st.write("Predicted Class:", predicted_class)
+    classes = ['Crazing', 'Inclusion', 'Patches', 'Pitted', 'Rolled', 'Scratches']
+    predicted_class_name = classes[predicted_class]
+
+    # Display the image and prediction
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+    st.write("Class Prediction: ", predicted_class_name)
+    st.write("Confidence: {:.2f}%".format(prediction[0][predicted_class] * 100))
+
+    # Bar chart for class probabilities
+    fig, ax = plt.subplots()
+    ax.bar(classes, prediction[0] * 100)
+    ax.set_ylabel('Probability (%)')
+    ax.set_title('Class Probabilities')
+    st.pyplot(fig)
