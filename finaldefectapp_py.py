c5ed5d2c7fd83821913fc5c7786dc859a7e6ee7d
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-11-29 19:34:52 +0200

    Update finaldefectapp_py.py

--- finaldefectapp_py.py
+++ finaldefectapp_py.py
@@ -1,55 +1,48 @@
 # Import necessary libraries
 from keras.preprocessing import image
-from keras.models import load_model
+from keras.applications.mobilenet import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
 from PIL import Image
 
 # Load the trained MobileNet model
 model = 'mobilenet_model(1).h5'
 
-# Define the image size for model input
-img_width, img_height = 150, 150
-
-# Function for image preprocessing
+# Image preprocessing function
 def preprocess_image(img):
-    # Resize the image to the required dimensions
-    img = img.resize((img_width, img_height))
-    # Convert the image to a numpy array
+    img = img.resize((150, 150))  # Resize image to match the input size of the model
     img_array = image.img_to_array(img)
-    # Expand the dimensions to create a batch of size 1
     img_array = np.expand_dims(img_array, axis=0)
-    # Normalize pixel values to the range [0, 1]
-    img_array /= 255.0
+    img_array = preprocess_input(img_array)
     return img_array
 
-# Function for model prediction
-def predict_defect(image_path):
-    # Load and preprocess the image
-    img = Image.open(image_path)
-    img_array = preprocess_image(img)
-    
-    # Make predictions
-    predictions = model.predict(img_array)
-    # Get the class with the highest probability
-    predicted_class = np.argmax(predictions)
-    
-    return predicted_class
+# Prediction function
+def predict_defect(img_array):
+    prediction = model.predict(img_array)
+    return prediction
 
-# Streamlit user interface
-st.title("Defect Assessment App")
+# Streamlit UI
+st.title('Defects Assessment')
 
-# Upload image through the Streamlit interface
+# Upload image through Streamlit
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-# Display the uploaded image
 if uploaded_file is not None:
-    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+    # Display the uploaded image
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
 
-    # Make predictions when the "Predict" button is clicked
-    if st.button("Predict"):
-        # Get the predictions
-        prediction = predict_defect(uploaded_file)
+    # Preprocess the image
+    img_array = preprocess_image(img)
 
-        # Display the prediction result
-        defect_classes = ["Crazing", "Inclusion", "Patches", "Pitted", "Rolled", "Scratches"]
-        result = f"The predicted defect class is: {defect_classes[prediction]}"
-        st.success(result)
+    # Make a prediction
+    prediction = predict_defect(img_array)
+
+    # Display the prediction result
+    st.write("Prediction:")
+    st.write(prediction)
+
+    # You can further process the prediction result to display a meaningful output based on your classes.
+    # For example, if you have classes like ['Crazing', 'Inclusion', 'Patches', 'Pitted', 'Rolled', 'Scratches'],
+    # you can display the predicted class with the highest probability.
+    predicted_class = np.argmax(prediction)
+    st.write("Predicted Class:", predicted_class)
