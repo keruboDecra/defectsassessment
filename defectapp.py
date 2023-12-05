67c9ae697cdf8cf86dde7204aeb73c5889f7ee6d
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-05 08:07:26 +0200

    Update defectapp.py

--- defectapp.py
+++ defectapp.py
@@ -37,7 +37,7 @@ def main():
     st.title("Defects Assessment App")
 
     # Display sample images
-    sample_images = ['Crazing.bmp', 'Inclusion.jpg', 'Patches.bmp', 'Pitted.bmp', 'Rolled.jpg', 'Scratches.bmp']
+    sample_images = ['Crazing.bmp', 'inclusion.jpg', 'Patches.bmp', 'Pitted.bmp', 'Rolled.jpg', 'Scratches.bmp']
     for sample_image in sample_images:
         st.image(sample_image, caption=sample_image, use_column_width=True)
 
