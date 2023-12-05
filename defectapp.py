326e99a210db953a40b477981ce1925082abe514
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-05 07:05:46 +0200

    Update defectapp.py

--- defectapp.py
+++ defectapp.py
@@ -18,8 +18,7 @@ def load_mobilenet_model():
         return None
 
 # Function to make predictions
-def predict_defect(image_path, model):
-    img = image.load_img(image_path, target_size=(150, 150))
+def predict_defect(img, model):
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) / 255.0
 
@@ -27,10 +26,15 @@ def predict_defect(image_path, model):
     return prediction
 
 # Function to assess the highest probability predicted and print out the class of the image
-def assess_defect(prediction, classes):
+def assess_defect(prediction, classes, threshold):
     max_prob_index = np.argmax(prediction)
     max_prob_class = classes[max_prob_index]
-    return max_prob_class
+
+    # Check if the max probability is above the threshold
+    if max(prediction[0]) >= threshold:
+        return max_prob_class
+    else:
+        return "No relevant defect found"
 
 # Streamlit App
 def main():
@@ -42,37 +46,23 @@ def main():
     if uploaded_file is not None:
         st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
 
-        # Create a temporary directory if it doesn't exist
-        temp_dir = 'temp'
-        os.makedirs(temp_dir, exist_ok=True)
-
-        # Create a path for the temporary image
-        temp_path = os.path.join(temp_dir, 'temp_image.jpg')
-        uploaded_file.seek(0)
-        with open(temp_path, 'wb') as f:
-            f.write(uploaded_file.read())
-
         # Load the model
         model = load_mobilenet_model()
 
         if model is not None:
             # Make predictions
-            prediction = predict_defect(temp_path, model)
+            img = image.load_img(uploaded_file, target_size=(150, 150))
+            prediction = predict_defect(img, model)
 
             # Display the results
             st.subheader("Prediction Results:")
             for i, class_name in enumerate(classes):
                 st.write(f"{class_name}: {prediction[0][i]}")
 
-            # Assess the highest probability predicted and print out the class
-            max_prob_class = assess_defect(prediction[0], classes)
+            # Set a threshold for alerting
+            threshold = st.slider("Set Threshold", 0.0, 1.0, 0.95, 0.01)
+            max_prob_class = assess_defect(prediction[0], classes, threshold)
             st.success(f"This metal surface has a defect of: {max_prob_class}")
-
-            # Set a threshold for alerting with an interactive slider
-            threshold = st.slider("Set the threshold", min_value=0.0, max_value=1.0, value=0.95, step=0.01)
-            max_prob = max(prediction[0])
-            if max_prob < threshold:
-                st.warning("No relevant defect found. Please check the image again.")
 
 # Define your classes
 classes = ['Crazing', 'Inclusion', 'Patches', 'Pitted', 'Rolled', 'Scratches']
